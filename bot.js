d23384c751fb62908119b541733cb4a2c720d21a
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2020-04-21 15:34:07 +0300

    added "kick" command

--- bot.js
+++ bot.js
@@ -23,6 +23,13 @@ let db;
 })();
 
 
+async function delay(ms) {
+	return new Promise(resolve =>
+		setTimeout(() => resolve(), ms)
+	);
+}
+
+
 client.on('ready', () => {
 	console.log('Ready');
 });
@@ -36,12 +43,13 @@ client.on('message', async message => {
 		const guild = channel.guild;
 		const channels = guild.channels;
 		
+		//await channel.startTyping();
 		if (channel.name === 'room-bot') {
 			// Room commands
 			if (content.includes('room')) {
-				// Create room
 				const args = content.split(' ');
-				if (args[1] === 'create') {
+				// Create room
+				if (args[1] === 'open') {
 					// Getting id for the new channel
 					rows = await db.get(`select seq
 					                     from sqlite_sequence
@@ -55,7 +63,7 @@ client.on('message', async message => {
 					// Checking if saved category still exists
 					if (!category) {
 						// Creating category if not
-						channel.send('How did you not have any private rooms yet!?  :open_mouth:\n' +
+						channel.send('How did you not have any private rooms yet!?\n' +
 							'Nevermind, I even created the category for you! :kissing_heart:');
 						category = await channels.create('Private rooms by roomBot', {
 							type: 'category'
@@ -68,7 +76,7 @@ client.on('message', async message => {
 					// Creating channel
 					const room = await channels.create(`Private room ${id}`, {
 						type: 'text',
-						topic: 'Private room created by privateBot!  :smiling_face_with_3_hearts:',
+						topic: `Private room created by privateBot! Owner: ${message.author.username}  :smiling_face_with_3_hearts:`,
 						parent: category,
 						permissionOverwrites: [
 							{id: message.author.id, allow: 'VIEW_CHANNEL'},
@@ -79,16 +87,15 @@ client.on('message', async message => {
 					        values ($cid, $oid)`, {$cid: room.id, $oid: message.author.id});
 					channel.send(`There you go! Created room ${id} just for you!  :relaxed:`);
 				}
-				
-				// Add user to the room
-				if (args[2] === 'add') {
+				// Add or remove users from the room
+				else if (args[2] === 'add' || args[2] === 'kick') {
 					const roomId = args[1];
 					const username = args[3];
 					
-					let rows = await db.get(`select channel_snowflake as sf
+					let rows = await db.get(`select channel_snowflake as csf
 					                         from rooms
 					                         where id = $rid`, {$rid: roomId});
-					const room = await channels.resolve(rows ? rows['sf'] : null);
+					const room = await channels.resolve(rows ? rows['csf'] : null);
 					const user = (await guild.members.fetch()).find(usr =>
 						(usr.user.username.toLowerCase() === username.toLowerCase()) ||
 						(usr.nickname ? usr.nickname.toLowerCase() === username.toLowerCase() : false));
@@ -98,53 +105,74 @@ client.on('message', async message => {
 					} else if (!user) {
 						channel.send(`You want to chat with ${username}? Can\'t find any ${username} in here  :worried:`)
 					} else {
-						rows = await db.get(`select owner_snowflake as owner
+						rows = await db.get(`select owner_snowflake as osf
 						                     from rooms
 						                     where channel_snowflake = $cid`, {$cid: room.id});
-						if (rows['owner'] !== message.author.id) {
-							const owner = await guild.members.resolve(rows['owner']);
+						if (rows['osf'] !== message.author.id) {
+							const owner = await guild.members.resolve(rows['osf']);
 							channel.send('Someone is trying to mess with your room!!!  :sos: :sos: :sos:', {
 								reply: owner.user
 							});
-						} else if (user.user.id === message.author.id) {
-							channel.send('Have you decided to add yourself again? Don\'t worry, ' +
-								'you\'re already there!  :wink:')
-						} else {
-							rows = await db.all(`select owner_snowflake as owner, room_users.user_snowflake as user
-							                     from rooms
-								                          left join room_users on rooms.id = room_users.room_id
-							                     where channel_snowflake = $cid`, {$cid: room.id});
-							await room.updateOverwrite(user, {'VIEW_CHANNEL': true});
-							if (rows.some(id => id === user.user.id)) {
+						}
+						
+						rows = await db.get(`select owner_snowflake, room_users.user_snowflake
+						                     from rooms
+							                          left join room_users on rooms.id = room_users.room_id
+						                     where channel_snowflake = $cid
+							                   and user_snowflake = $uid`, {$cid: room.id, $uid: user.user.id});
+						if (args[2] === 'add') {  // Adding user
+							
+							if (user.user.id === message.author.id) {
+								channel.send('Have you decided to add yourself again? Don\'t worry, ' +
+									'you\'re already there!  :wink:')
+							} else if (rows) {
 								channel.send('You\'re trying to add the same person again. Just go to the channel, ' +
 									'they\'re waiting for you there!');
 							} else {
 								db.run(`insert into room_users(room_id, user_snowflake)
-								        values($rid, $uid)`, {$rid: roomId, $uid: user.user.id});
-								channel.send('Done, enjoy!  :fireworks:');
+								        values ($rid, $uid)`, {$rid: roomId, $uid: user.user.id});
+								await room.updateOverwrite(user, {'VIEW_CHANNEL': true});
+								channel.send('Done, enjoy their company!  :fireworks:');
+							}
+						} else {
+							if (user.user.id === message.author.id) {
+								channel.send('Wait, if you remove yourself, then who\'s left to manage your room?  ' +
+									':scream:\nDon\'t even think about that! We need you!')
+							} else if (!rows) {
+								channel.send('You\'re trying to kick a person who\'s even not in the room! ' +
+									'Why do you hate them so much!?  :frowning:')
+							} else {
+								db.run(`delete
+								        from room_users
+								        where room_id = $rid
+									      and user_snowflake = $uid`, {$rid: roomId, $uid: user.user.id});
+								await room.permissionOverwrites.get(user.user.id).delete();
+								channel.send('Done! They won\'t bother you ever again!  :smiling_imp:  ' +
+									'(well, at least, in that room)');
 							}
 						}
 					}
 				}
-				
 				// Remove the room
-				if (args[1] === 'remove') {
+				else if (args[2] === 'close') {
 					channel.send('I can\'t do this just yet.  :cry:  But I will, I promise!');
 				}
 				
-				// Remove user from the room
-				if (args[2] === 'del') {
-					channel.send('I can\'t do this just yet.  :cry:  But I will, I promise!');
+				// Commands not recognized by the bot
+				else {
+					channel.send(`:face_with_raised_eyebrow:  I am just a small room bot, what do you want from me?`);
 				}
 			}
 			
 			// Clear command
-			if (content.includes('clear')) {
+			else if (content.includes('clear')) {
 				await channel.bulkDelete(100);
 				channel.send(`Enjoy your clear channel now, ${message.author.username}!  :relieved:`);
 			}
+			
 		} else {
 			channel.send('Were you looking for me? Ugh, I have no powers here...  :sob:  Meet me in my channel!')
 		}
+		//channel.stopTyping();
 	}
 });
